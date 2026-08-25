041c2399e7e5a04bd5e5b9eec36459f4b658a591
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2026-08-25 18:40:57 +0200

    basic gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,21 +3,26 @@
 
 function GameBoard () {
 
-  let board = []
+  let board = createBoard()
+  let missed = 0
 
   function createBoard () {
 
-    board = []
+    let newBoard = []
 
     for (let x = 0; x < 10; x++) {
-      board.push([])
+      newBoard.push([])
       for (let y = 0; y < 10; y++) {
-        board[x].push(0)
+        newBoard[x].push(0)
       }
     }
 
-    return board
+    return newBoard
 
+  }
+
+  function getBoard() {
+    return board
   }
 
   function placeShip (location, ship) {
@@ -30,11 +35,35 @@ function GameBoard () {
 
   }
 
-  function receiveAttack () {
+  function receiveAttack (location) {
+
+    const [y, x] = location
+
+    if (board[y][x] === 1){
+      return "hit"
+    }
+    missed++
+
+    return [y, x]
 
   }
 
- return {createBoard, placeShip}
+  function displayMiss () {
+    return missed
+  }
+
+  function shipsSunk () {
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[row].length; col++) {
+        if (board[row][col] == 1) {
+          return false
+        };
+      }
+    }
+    return true
+  }
+
+ return {createBoard, placeShip, receiveAttack, displayMiss, getBoard, shipsSunk}
 }
 
 export {GameBoard}
